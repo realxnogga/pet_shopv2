0006863397d074d6d0645d7999ac0ecd17909448
realxnogga <larioswarren5@gmail.com>
2024-06-02 18:50:53 +0800

    to be continued

--- src/page/admin/adminproduct.jsx
+++ src/page/admin/adminproduct.jsx
@@ -99,7 +99,7 @@ export const AdminInventory = () => {
 
 
     const [selectedProduct, setSelectedProduct] = useState({});
-    const handleEditProductFunc = (productID) => { 
+    const handleEditProductFunc = (productID) => {
         let selectedProductTemp = fetchedProductData.find(item => item.productID === productID);
         if (selectedProductTemp) {
             setSelectedProduct(selectedProductTemp);
@@ -108,7 +108,7 @@ export const AdminInventory = () => {
 
     const handleEditInputChange = (e) => {
         const { name, value } = e.target;
-        setSelectedProduct({...selectedProduct, [name]: value,});
+        setSelectedProduct({ ...selectedProduct, [name]: value, });
     };
 
     const [productPic, setProductPic] = useState(null);
@@ -116,12 +116,12 @@ export const AdminInventory = () => {
         setProductPic(e.target.files[0]);
     }
 
-    console.log(productPic);    
+    console.log(productPic);
 
     const handleEditInputFunc = () => {
-        dispatch(UpdateProductThunk({selectedProduct, productPic}));   
+        dispatch(UpdateProductThunk({ selectedProduct, productPic }));
     }
-     
+
     const isAdminProductUpdated = useSelector(isAdminProductUpdatedTemp);
 
     useEffect(() => {
@@ -145,97 +145,6 @@ export const AdminInventory = () => {
             <AdminHamburger />
 
             <div className="h-[90%] w-[70rem] flex flex-col gap-y-4">
-
-                {/* edit product modal */}
-                <dialog id="editProductModal" className="modal">
-
-                    <div className="modal-box h-fit w-[25rem] flex flex-col gap-y-4 rounded-lg p-4 noScrollbar">
-
-                        <div className="w-full">
-                            <label className={`text-lg text-black`}>Enter product name<span className='text-red-500'>*</span></label>
-                            <input
-                                onChange={handleEditInputChange}
-                                value={selectedProduct.productname}
-                                name="productname"
-                                type="text" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
-                        </div>
-
-                        <div className="w-full">
-                            <label className={`text-lg text-black`}>Choose product size<span className='text-red-500'>*</span></label>
-                            <select
-                                onChange={handleEditInputChange}
-                                value={selectedProduct.productsize}
-                                name="productsize"
-                                className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
-                                <option value="XS">XS</option>
-                                <option value="S">S</option>
-                                <option value="M">M</option>
-                                <option value="L">L</option>
-                                <option value="XL">XL</option>
-                            </select>
-                        </div>
-
-                        <div className="w-full flex gap-x-4">
-                            <div>
-                                <label className={`text-lg text-black`}>Enter product stock<span className='text-red-500'>*</span></label>
-                                <input
-                                    onChange={handleEditInputChange}
-                                    value={selectedProduct.productstock}
-                                    name="productstock"
-                                    type="number" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
-                            </div>
-                            <div>
-                                <label className={`text-lg text-black`}>Enter product price<span className='text-red-500'>*</span></label>
-                                <input
-                                    onChange={handleEditInputChange}
-                                    value={selectedProduct.productprice}
-                                    name="productprice"
-                                    type="number" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label className={`text-lg text-black`}>Enter product description<span className='text-red-500'>*</span></label>
-                            <textarea
-                                onChange={handleEditInputChange}
-                                value={selectedProduct.productdescription}
-                                name="productdescription"
-                                className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
-                            </textarea>
-                        </div>
-
-                        <div className="w-full">
-                            <label className={`text-lg text-black`}>Choose product category<span className='text-red-500'>*</span></label>
-                            <select
-                                onChange={handleEditInputChange}
-                                value={selectedProduct.productcategory}
-                                name="productcategory"
-                                className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
-                                <option value="Dog Food">Dog Food</option>
-                                <option value="Cat Food">Cat Food</option>
-                                <option value="Bird Food">Bird Food</option>
-                                <option value="Vitamins/Supplements">Vitamins/Supplements</option>
-                                <option value="Flea/Tick Control">Flea/Tick Control</option>
-                                <option value="Grooming Supplies">Grooming Supplies</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label className={`text-lg text-black`}>Enter product image<span className='text-red-500'>*</span></label>
-                            <input onChange={handleProductPicChange} id="fileInput" type="file" className={`p-2 border border-gray-400 rounded-sm w-full outline-none text-black text-md`} />
-                        </div>
-
-                        <div className="w-full flex gap-x-3">
-                            <button onClick={handleEditInputFunc} className="bg-green-500 hover:bg-green-400 py-2 px-4 rounded-sm text-white flex items-center gap-x-1">Edit</button>
-                        </div>
-
-                    </div>
-
-                    <form method="dialog" className="modal-backdrop">
-                        <button>close</button>
-                    </form>
-                </dialog>
-
 
 
                 <div className="w-full flex justify-end ">
@@ -275,7 +184,7 @@ export const AdminInventory = () => {
                                 <td className="border px-2">{item.creationdate}</td>
                                 <td className="border px-2 min-w-[6rem]">
                                     <div className="flex items-center justify-evenly w-full h-full ">
-                                        <MdEdit onClick={() => {handleEditProductFunc(item.productID); document.getElementById('editProductModal').showModal()}} data-type="info" data-tip="Edit" className="text-3xl p-1 bg-green-500 hover:bg-green-400 rounded-sm text-white" />
+                                        <MdEdit onClick={() => { handleEditProductFunc(item.productID); document.getElementById('editProductModal').showModal() }} data-type="info" data-tip="Edit" className="text-3xl p-1 bg-green-500 hover:bg-green-400 rounded-sm text-white" />
                                         <RiDeleteBin5Fill onClick={() => { DeleteProductFunc(item.productID, item.productimage) }} data-type="info" data-tip="Delete" className="text-3xl p-1 bg-red-500 hover:bg-red-400 rounded-sm text-white" />
                                         <ReactTooltip />
                                     </div>
@@ -287,6 +196,96 @@ export const AdminInventory = () => {
 
                 </div>
             </div>
+
+            {/* edit product modal */}
+            <dialog id="editProductModal" className="modal">
+
+                <div className="modal-box h-fit w-[25rem] flex flex-col gap-y-4 rounded-lg p-4 noScrollbar">
+
+                    <div className="w-full">
+                        <label className={`text-lg text-black`}>Enter product name<span className='text-red-500'>*</span></label>
+                        <input
+                            onChange={handleEditInputChange}
+                            value={selectedProduct.productname}
+                            name="productname"
+                            type="text" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
+                    </div>
+
+                    <div className="w-full">
+                        <label className={`text-lg text-black`}>Choose product size<span className='text-red-500'>*</span></label>
+                        <select
+                            onChange={handleEditInputChange}
+                            value={selectedProduct.productsize}
+                            name="productsize"
+                            className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
+                            <option value="XS">XS</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                        </select>
+                    </div>
+
+                    <div className="w-full flex gap-x-4">
+                        <div>
+                            <label className={`text-lg text-black`}>Enter product stock<span className='text-red-500'>*</span></label>
+                            <input
+                                onChange={handleEditInputChange}
+                                value={selectedProduct.productstock}
+                                name="productstock"
+                                type="number" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
+                        </div>
+                        <div>
+                            <label className={`text-lg text-black`}>Enter product price<span className='text-red-500'>*</span></label>
+                            <input
+                                onChange={handleEditInputChange}
+                                value={selectedProduct.productprice}
+                                name="productprice"
+                                type="number" className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className={`text-lg text-black`}>Enter product description<span className='text-red-500'>*</span></label>
+                        <textarea
+                            onChange={handleEditInputChange}
+                            value={selectedProduct.productdescription}
+                            name="productdescription"
+                            className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
+                        </textarea>
+                    </div>
+
+                    <div className="w-full">
+                        <label className={`text-lg text-black`}>Choose product category<span className='text-red-500'>*</span></label>
+                        <select
+                            onChange={handleEditInputChange}
+                            value={selectedProduct.productcategory}
+                            name="productcategory"
+                            className={`border border-gray-400 rounded-sm w-full outline-none p-2 text-black text-md`}>
+                            <option value="Dog Food">Dog Food</option>
+                            <option value="Cat Food">Cat Food</option>
+                            <option value="Bird Food">Bird Food</option>
+                            <option value="Vitamins/Supplements">Vitamins/Supplements</option>
+                            <option value="Flea/Tick Control">Flea/Tick Control</option>
+                            <option value="Grooming Supplies">Grooming Supplies</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className={`text-lg text-black`}>Enter product image<span className='text-red-500'>*</span></label>
+                        <input onChange={handleProductPicChange} id="fileInput" type="file" className={`p-2 border border-gray-400 rounded-sm w-full outline-none text-black text-md`} />
+                    </div>
+
+                    <div className="w-full flex gap-x-3">
+                        <button onClick={handleEditInputFunc} className="bg-green-500 hover:bg-green-400 py-2 px-4 rounded-sm text-white flex items-center gap-x-1">Edit</button>
+                    </div>
+
+                </div>
+
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
 
             {/* add product modal */}
             <dialog id="addProductModal" className="modal">
