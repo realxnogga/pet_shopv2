9bf08536466426bce9617aa52732478db7b9f213
realxnogga <larioswarren5@gmail.com>
2024-06-05 19:37:47 +0800

    add database

--- server/petshop.sql
+++ server/petshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2024 at 07:54 PM
+-- Generation Time: Jun 05, 2024 at 01:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -28,32 +28,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `addtocartproduct` (
+  `addtocartprimarykey` int(11) NOT NULL,
   `addtocartproductID` int(11) NOT NULL,
   `clientID` int(11) NOT NULL,
   `clientusername` varchar(225) NOT NULL,
   `addtocartproductname` varchar(225) NOT NULL,
   `addtocartproductsize` text NOT NULL,
-  `addtocartproductquantity` int(11) NOT NULL,
-  `addtocartproducttotalprice` int(11) NOT NULL,
-  `addtocartdate` datetime NOT NULL DEFAULT current_timestamp(),
-  `clientaddress` varchar(225) NOT NULL
+  `addtocartproductstock` int(11) NOT NULL,
+  `addtocartproductprice` int(11) NOT NULL,
+  `addtocartproductdescription` varchar(225) NOT NULL,
+  `addtocartproductcategory` text NOT NULL,
+  `addtocartproductcreationdate` datetime NOT NULL DEFAULT current_timestamp(),
+  `addtocartproductimage` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `addtocartproduct`
 --
 
-INSERT INTO `addtocartproduct` (`addtocartproductID`, `clientID`, `clientusername`, `addtocartproductname`, `addtocartproductsize`, `addtocartproductquantity`, `addtocartproducttotalprice`, `addtocartdate`, `clientaddress`) VALUES
-(1, 0, '', '', '', 0, 0, '2024-06-05 00:30:49', ''),
-(2, 0, '', '', '', 0, 0, '2024-06-05 00:31:18', ''),
-(3, 11, 'warren', 'Pedegree', 'M', 1, 100, '2024-06-05 00:32:06', 'brgy puypuy'),
-(4, 11, 'warren', 'Pedegree', 'M', 1, 100, '2024-06-05 00:33:04', 'brgy puypuy'),
-(5, 11, 'warren', 'Tick Free', 'L', 2, 200, '2024-06-05 00:56:11', 'brgy puypuy'),
-(6, 11, 'warren', 'Tick Free', 'L', 1, 100, '2024-06-05 00:58:12', 'brgy puypuy'),
-(7, 11, 'warren', 'Tick Free', 'L', 1, 100, '2024-06-05 01:20:37', 'brgy puypuy'),
-(8, 11, 'warren', 'Pedegree', 'M', 1, 100, '2024-06-05 01:23:39', 'brgy puypuy'),
-(9, 9, 'manolo', 'Tick Free', 'L', 2, 200, '2024-06-05 01:29:06', 'brgy san isidro'),
-(10, 9, 'manolo', 'sample', 'M', 1, 5, '2024-06-05 01:43:02', 'brgy san isidro');
+INSERT INTO `addtocartproduct` (`addtocartprimarykey`, `addtocartproductID`, `clientID`, `clientusername`, `addtocartproductname`, `addtocartproductsize`, `addtocartproductstock`, `addtocartproductprice`, `addtocartproductdescription`, `addtocartproductcategory`, `addtocartproductcreationdate`, `addtocartproductimage`) VALUES
+(1, 36, 12, 'warren', 'Tick Free', 'L', 130, 100, 'sample description', 'Flea/Tick Control', '2024-06-05 19:32:40', 'tickfree.jpg_20240604091348'),
+(2, 42, 12, 'warren', 'Pedegree', 'XL', 184, 200, 'sample', 'Dog Food', '2024-06-05 19:32:46', 'pedegree.jpg_20240605133042');
 
 -- --------------------------------------------------------
 
@@ -97,9 +92,8 @@ CREATE TABLE `adminproduct` (
 --
 
 INSERT INTO `adminproduct` (`productID`, `productname`, `productsize`, `productstock`, `productprice`, `productdescription`, `productcategory`, `creationdate`, `productimage`) VALUES
-(35, 'Pedegree', 'M', 96, 100, 'sample description', 'Dog Food', '2024-06-04 15:12:24', 'pedegree.jpg_20240604091224'),
-(36, 'Tick Free', 'L', 148, 100, 'sample description', 'Flea/Tick Control', '2024-06-04 15:13:48', 'tickfree.jpg_20240604091348'),
-(38, 'sample', 'M', 5, 5, 'sample', 'Dog Food', '2024-06-05 01:37:18', 'drink.jpg_20240604193718');
+(36, 'Tick Free', 'L', 130, 100, 'sample description', 'Flea/Tick Control', '2024-06-04 15:13:48', 'tickfree.jpg_20240604091348'),
+(42, 'Pedegree', 'XL', 182, 200, 'sample', 'Dog Food', '2024-06-05 19:30:42', 'pedegree.jpg_20240605133042');
 
 -- --------------------------------------------------------
 
@@ -108,6 +102,7 @@ INSERT INTO `adminproduct` (`productID`, `productname`, `productsize`, `products
 --
 
 CREATE TABLE `buyproduct` (
+  `buyproductprimarykey` int(11) NOT NULL,
   `buyproductID` int(11) NOT NULL,
   `clientID` int(11) NOT NULL,
   `clientusername` varchar(225) NOT NULL,
@@ -124,17 +119,10 @@ CREATE TABLE `buyproduct` (
 -- Dumping data for table `buyproduct`
 --
 
-INSERT INTO `buyproduct` (`buyproductID`, `clientID`, `clientusername`, `productname`, `productsize`, `productquantity`, `producttotalprice`, `clientaddress`, `orderdate`, `orderstatus`) VALUES
-(3, 2, 'real', 'pedegree', 'M', 2, 800, '', '2024-06-02 10:29:12', 'not recieve'),
-(4, 2, 'real', 'fgfg', 'XS', 2, 10, '', '2024-06-02 10:29:12', 'not recieve'),
-(51, 9, 'manolo', 'Tick Free', 'M', 2, 140, 'brgy san isidro', '2024-06-02 20:32:49', 'not recieve'),
-(52, 9, 'manolo', 'Pedegree', 'M', 1, 150, 'brgy san isidro', '2024-06-02 20:37:09', 'not recieve'),
-(53, 9, 'manolo', 'Pedegree', 'M', 1, 150, 'brgy san isidro', '2024-06-02 20:37:28', 'not recieve'),
-(54, 10, 'warren', 'Pedegree', 'M', 1, 150, 'brgy puypuy', '2024-06-02 20:39:29', 'recieve'),
-(55, 10, 'warren', 'Tick Free', 'M', 1, 70, 'brgy puypuy', '2024-06-02 20:39:32', 'not recieve'),
-(63, 11, 'warren', 'Pedegree', 'M', 1, 100, 'brgy puypuy', '2024-06-04 23:41:35', 'not recieve'),
-(64, 11, 'warren', 'Pedegree', 'M', 3, 300, 'brgy puypuy', '2024-06-04 23:51:36', 'not recieve'),
-(65, 11, 'warren', 'Tick Free', 'L', 2, 200, 'brgy puypuy', '2024-06-04 23:51:51', 'not recieve');
+INSERT INTO `buyproduct` (`buyproductprimarykey`, `buyproductID`, `clientID`, `clientusername`, `productname`, `productsize`, `productquantity`, `producttotalprice`, `clientaddress`, `orderdate`, `orderstatus`) VALUES
+(1, 36, 12, 'warren', 'Tick Free', 'L', 1, 100, 'brgy san isidro', '2024-06-05 19:32:26', 'not recieve'),
+(2, 42, 12, 'warren', 'Pedegree', 'XL', 5, 1000, 'brgy san isidro', '2024-06-05 19:32:34', 'not recieve'),
+(3, 42, 12, 'warren', 'Pedegree', 'XL', 2, 400, 'brgy san isidro', '2024-06-05 19:33:09', 'recieve');
 
 -- --------------------------------------------------------
 
@@ -156,8 +144,7 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`clientID`, `clientusername`, `clientpassword`, `clientaddress`, `joindate`, `clientprofile`) VALUES
-(9, 'manolo', 'manolo123', 'brgy san isidro', '2024-06-02 20:22:15', 'tickfree.jpg_20240602142215'),
-(11, 'warren', 'warren123', 'brgy puypuy', '2024-06-02 20:56:07', 'drink.jpg_20240602145607');
+(12, 'warren', 'warren123', 'brgy san isidro', '2024-06-05 18:51:49', 'drink.jpg_20240605125149');
 
 --
 -- Indexes for dumped tables
@@ -167,7 +154,7 @@ INSERT INTO `client` (`clientID`, `clientusername`, `clientpassword`, `clientadd
 -- Indexes for table `addtocartproduct`
 --
 ALTER TABLE `addtocartproduct`
-  ADD PRIMARY KEY (`addtocartproductID`);
+  ADD PRIMARY KEY (`addtocartprimarykey`);
 
 --
 -- Indexes for table `admin`
@@ -185,7 +172,7 @@ ALTER TABLE `adminproduct`
 -- Indexes for table `buyproduct`
 --
 ALTER TABLE `buyproduct`
-  ADD PRIMARY KEY (`buyproductID`);
+  ADD PRIMARY KEY (`buyproductprimarykey`);
 
 --
 -- Indexes for table `client`
@@ -201,7 +188,7 @@ ALTER TABLE `client`
 -- AUTO_INCREMENT for table `addtocartproduct`
 --
 ALTER TABLE `addtocartproduct`
-  MODIFY `addtocartproductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `addtocartprimarykey` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `admin`
@@ -213,19 +200,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `adminproduct`
 --
 ALTER TABLE `adminproduct`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `buyproduct`
 --
 ALTER TABLE `buyproduct`
-  MODIFY `buyproductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `buyproductprimarykey` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `clientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `clientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
